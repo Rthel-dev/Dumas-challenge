27ed91de3a1cf9bb0e40a5edaa2ca0d0e39fe123
Rodrigo Castillo C. <rodrigo.castillo.c@usach.cl>
2026-03-13 00:53:52 -0300

    API: Added configuration for enabled cors and allowed origins sites

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(','),
+    credentials: true
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
