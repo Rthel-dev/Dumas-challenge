110343b60a50b209441024e31ec734608b2ce925
Rodrigo Castillo C. <rodrigo.castillo.c@usach.cl>
2026-03-13 17:47:51 -0300

    API: Added new tests for Tasks service and controller

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { AuthModule } from '../auth/auth.module';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [AuthModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [JwtAuthGuard, TasksService],
   exports: [TasksService],
 })
 export class TasksModule {}
